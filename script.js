c236043bda5fef452ec800a47344178bef483abf
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-11-07 15:58:46 +0100

    feat: mandato avanti parte dell'esercizio

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+//creazione delle variabili che poi utilizzeremo
+const numRandom = document.querySelector("h1")
+const numPrimo = document.getElementById("num-1")
+const numSecond = document.getElementById("num-2")
+const numTerzo = document.getElementById("num-3")
+const numQuarto = document.getElementById("num-4")
+const numQuinto = document.getElementById("num-5")
+const btnResult = document.getElementById("submitBtn")
+const risultato = document.getElementById("result")
+let arrayNumerico = ["20","40","25","45","30"]
+console.log
+//processo creativo: 
+// 1)creare un modo per andare a fare il display in pagina di cinque numeri in maniera randomica
+// 2) dopo averli messi in display l'utente inserisce i numeri che si ricorda nelle apposite text area e preme invio sul bottone
+// 3) dopo che ha premuto invio, il computer vede quali sono i numeri che ha azzeccato l'utente e poi gli dice quelli che sono stati azzeccati
